4fa3cbe5756047098f085978abf489dcebf406b6
vishalpatil0 <vishalgpatil10@gmail.com>
2020-10-29 10:51:03 +0530

    modified folder

--- How to create exe file using cx_Freeze/setup.py
+++ How to create exe file using cx_Freeze/setup.py
@@ -2,5 +2,6 @@ from cx_Freeze import setup, Executable
 setup(name="vishal",
       version="0.1",
       description="ok",
-      executables=[Executable(r"stone-paper-scisor1.py")]
+      executables=[Executable(r"stone-paper-scisor1.py",base = "Win32GUI")]
       )
+#this base="Win32GUI" hides the console window
