ff7a892bd6b787855876f6c3c13aaeead9bd7177
vishalpatil0 <vishalgpatil10@gmail.com>
2020-11-07 15:11:55 +0530

    Playsound Module

---
+++ Playsound Module.py
@@ -0,0 +1,10 @@
+import playsound
+
+playsound.playsound(r"C:\Users\Vishal\Desktop\git\Basic-Snake-Game\sounds\background.mp3")
+
+
+
+"""
+NOTE: This module has only one method and this playsound method have only one paramaeter that is sound file.
+      You can only play .wav and .mp3 file.
+"""
